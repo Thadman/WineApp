a576f32187043f7b3f89291f611c84ad0f62604d
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-12 22:29:27 +1000

    added html files and added controller logic, still need to fox seeding problem

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,9 +14,34 @@ class ListingsController < ApplicationController
     @listing = Listing.new
   end
 
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.save
+
+    redirect_to @listing
+  end
+
+  def edit 
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params)
+
+    redirect_to @listing
+  end
+
+  def destroy
+    @listing.destroy
+
+    redirect_to listings_path
+  end
+
   private
 
   def listing_params
     params.require(:listing).permit(:name, :vintage, :region, :grape, :notes, :description, :alcohol, :size, :wine_type, :price)
   end
 end
+
