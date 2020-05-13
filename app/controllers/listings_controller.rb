35ad43af43070cb58d0dfb0d7c117d5ca2caa0f1
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-13 15:16:09 +1000

    added some photos for the seeding

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -42,5 +42,7 @@ class ListingsController < ApplicationController
   def listing_params
     params.require(:listing).permit(:name, :vintage, :region, :grape, :notes, :description, :alcohol, :size, :wine_type, :price)
   end
+
+  
 end
 
