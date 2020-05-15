bdb789e9460819caa1e8de82029802585ea79422
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-15 13:44:48 +1000

    added devise fields to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
   flash[:authorization_error] = "Current user not authorized to perform that action"
   redirect_to listings_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
+  end
+
 end
-
-
