0a5557132bf6083e1141e0e58a7ea464254bde9c
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-17 11:23:38 +1000

    added search ability, need to add some view logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def search
+    if params[:search].blank?
+      redirect_to(root_path, alert: "No results for this sorry!") and return
+    else
+      @parameter = params[:search].downcase
+      @results = Listing.all.where("lower(name) LIKE :search", search: @parameter)
+    end
   end
 end
