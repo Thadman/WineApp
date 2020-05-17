04876a9a5abd9a35787bfea73b74dfba1b8c1352
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-17 14:24:44 +1000

    added basic dashboard functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :listings
   
+  resources :dashboard, only: [:index]
+
   root to: 'welcome#index'
 
   get '/search' => 'pages#search', as: 'search_page'
