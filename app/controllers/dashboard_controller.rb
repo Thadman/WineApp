56061b954b94a66e756993424461316f89b72352
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-19 13:34:15 +1000

    fixing the show page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,10 @@ class DashboardController < ApplicationController
   def index
     @listing = current_user.listings.paginate(page: params[:page], per_page: 6)
   end
+
+  def destroy
+    # @current_user = current_user.find(params[:id]).destroy
+
+    redirect_to listings_path
+  end
 end
